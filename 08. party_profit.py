0b84fa7b856ee7e5d754ac86042f719c3e9a68c3
Ivan PETKOV <ivn5e7k0v@gmail.com>
2023-02-21 09:23:49 +0200

    ex

---
+++ 08. party_profit.py
@@ -0,0 +1,17 @@
+group_size = int(input())
+days = int(input())
+coins_count = 0
+for day in range(1, days + 1):
+    if day % 10 == 0:
+        group_size -= 2
+    if day % 15 == 0:
+        group_size += 5
+    if day % 3 == 0:
+        coins_count -= group_size * 3
+    if day % 5 == 0:
+        coins_count += group_size * 20
+        if day % 3 == 0:
+            coins_count -= group_size * 2
+    coins_count += 50 - (group_size * 2)
+print(f"{group_size} companions received {int(coins_count / group_size)} coins each.")
+
