396e651c897d912bff1245fc1787b0a4f5ec037d
Ivan PETKOV <ivn5e7k0v@gmail.com>
2023-02-23 18:52:34 +0200

    exercise

---
+++ 10. gladiator_expenses.py
@@ -0,0 +1,18 @@
+lost_fights_count = int(input())
+helmet_price = float(input())
+sword_price = float(input())
+shield_price = float(input())
+armor_price = float(input())
+expenses = 0
+
+for each_fight in range(1, lost_fights_count + 1):
+    if each_fight % 2 == 0:
+        expenses += helmet_price
+    if each_fight % 3 == 0:
+        expenses += sword_price
+    if each_fight % 6 == 0:
+        expenses += shield_price
+    if each_fight % 12 == 0:
+        expenses += armor_price
+print(f"Gladiator expenses: {expenses:.2f} aureus")
+
