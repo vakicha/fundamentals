ba3f801bb5940b703df4d1ae04cc087764657efd
Ivan PETKOV <ivn5e7k0v@gmail.com>
2023-03-09 19:06:31 +0200

    function exercise

---
+++ 02. add_and_subtract.py
@@ -0,0 +1,12 @@
+integer_1 = int(input())
+integer_2 = int(input())
+integer_3 = int(input())
+
+
+def add_and_subtract():
+    add = integer_1 + integer_2
+    subtract = add - integer_3
+    print(subtract)
+
+
+add_and_subtract()
